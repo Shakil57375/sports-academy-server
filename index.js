8c46a087625692ee6444720a876aea19b25a5bea
Shakil57375 <shakil57375@gmail.com>
2023-06-09 00:13:36 +0600

    admin and instructor functionality has been added

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lc40fqb.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -33,12 +33,64 @@ async function run() {
 
     const usersCollection = client.db("sportsAcademy").collection("users")
 
+
+    // post users
+
     app.post("/users", async (req, res)=>{
       const body = req.body
+      console.log(body);
+      const query = { email : body.email };
+      console.log(query);
+      const existingUSer = await usersCollection.findOne(query);
+      if (existingUSer) {
+        return res.send({ message: "user already exists" });
+      }
       const result = await usersCollection.insertOne(body)
       res.send(result)
     })
 
+    // make admin
+    app.patch("/users/admin/:id", async(req, res)=>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const updatedDoc = {
+        $set: {
+          role : "admin"
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updatedDoc)
+      res.send(result)
+    })
+
+    // make Instructor
+    app.patch("/users/instructor/:id", async(req, res)=>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const updatedDoc = {
+        $set: {
+          role : "instructor"
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updatedDoc)
+      res.send(result)
+    })
+
+
+    // get all the users
+
+    app.get("/users", async (req, res)=>{
+      const result = await usersCollection.find().toArray()
+      res.send(result)
+    })
+
+    // delete user
+
+    app.delete("/users/:id", async(req, res)=>{
+      const id = req.params.id
+      const query = {_id : new ObjectId(id)}
+      const result = await usersCollection.deleteOne(query)
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
